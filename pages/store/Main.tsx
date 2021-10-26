e487d7eb7adbf403c55dac244081928e72e82da8
Dykann Khaov <dykann.khaov@gmail.com>
2021-10-26 19:52:06 +0200

    finish integrating shopping part

--- pages/store/Main.tsx
+++ pages/store/Main.tsx
@@ -1,64 +1,100 @@
 const products = [
   {
-    name: 'MacBook Pro 2020',
     desc: 'Macbook Pro édition 2020, 13 pouces',
-    imgUrl: '/public/macbook.jpeg',
-    price: '1000€',
+    imgUrl: 'macbook-pro.png',
+    price: '1449.00€',
     rate: 5,
   },
   {
-    name: 'Iphone 13 Pro',
     desc: 'Iphone 13 Pro 128 Go, Bleu alpin',
-    imgUrl: '/public/iphone13.png',
-    price: '1000€',
+    imgUrl: 'iphone13.png',
+    price: '1159.00€',
     rate: 5,
   },
   {
-    name: 'Casque Bose',
-    desc: 'Casque Bose sans fil QuietComfort 35 II, de couleur grise avec réduction de bruit',
-    imgUrl: '/public/casquebose.jpeg',
-    price: '1000€',
+    desc: 'Casque Bose gris sans fil QuietComfort 35 II',
+    imgUrl: 'casque-bose.jpeg',
+    price: '299.99€',
     rate: 5,
   },
   {
-    name: 'Chaussures Nike',
-    desc: '',
-    imgUrl: '/public/nikeairforce1.jpeg',
-    price: '1000€',
+    desc: 'AirPods Pro',
+    imgUrl: 'airpods-pro.png',
+    price: '279.00€',
     rate: 5,
   },
   {
-    name: 'Maillot PSG',
     desc: 'Maillot de foot du PSG extérieur',
-    imgUrl: '/public/maillotpsg.jpeg',
-    price: '1000€',
+    imgUrl: 'maillot-psg.jpeg',
+    price: '89.99€',
     rate: '5',
   },
   {
-    name: 'Collection de Cartes Pokémon',
-    desc: 'Lot 3 Cartes Pokémon 1ere Edition 1996 Dracaufeu Florizarre Tortank Rare French',
-    imgUrl: '/public/cartes-pokemon.jpeg',
-    price: '1000€',
-    rate: 4.5,
-  },
-  {
-    name: 'Mercedes Classe C',
-    desc: '',
-    imgUrl: '/public/voituremercedes.jpeg',
-    price: '1000€',
+    desc: 'Nike Air Force 1 "07 bleu glacier',
+    imgUrl: 'air-force1.png',
+    price: '104.95€',
     rate: 5,
   },
   {
-    name: 'Aspirateur robot',
-    desc: '',
-    imgUrl: '/public/aspirateur-robot.jpeg',
-    price: '1000€',
+    desc: 'Lot de 3 Cartes Pokémon 1ere Edition 1996',
+    imgUrl: 'cartes-pokemon.png',
+    price: '350.00€',
+    rate: 4.5,
+  },
+  {
+    desc: 'Sac Louis Vuitton',
+    imgUrl: 'sac-lv.png',
+    price: '1299.00€',
+    rate: 5,
+  },
+  {
+    desc: 'Robot-Aspirateur, Roomba',
+    imgUrl: 'roomba-aspirateur.png',
+    price: '599.00€',
+    rate: 5,
+  },
+
+  {
+    desc: 'Souris Logitech G903',
+    imgUrl: 'souris-logitech.png',
+    price: '121.90€',
+    rate: 5,
+  },
+  {
+    desc: 'Equipement pour Cocktail',
+    imgUrl: 'cocktail-stuff.png',
+    price: '19.99€',
+    rate: 5,
+  },
+  {
+    desc: 'Peluche Poro league of legends',
+    imgUrl: 'poro.png',
+    price: '18.99€',
     rate: 5,
   },
 ]
 
-console.log(products)
-
 export default function Main() {
-  return <h1>HELLO WORLD</h1>
+  return (
+    <>
+      <div className="pt-4 pl-8">
+        <h1 className="font-bold text-2xl mb-1">Notre sélection pour vous</h1>
+        <hr className="border-gray-500"></hr>
+      </div>
+
+      <div className="flex flex-wrap pt-4 pl-8">
+        {products.map((item, id) => (
+          <ul key={id} className="flex flex-col border w-80 mb-3">
+            <img src={`/${item.imgUrl}`} style={{ height: '250px', width: '250px' }} className="self-center" />
+            <div className="pl-2 text-sm">
+              <p className="font-bold">{item.desc}</p>
+              <p>{item.rate}/5</p>
+              <p>{item.price}</p>
+            </div>
+            <button className="bg-green-600 text-white text-sm mt-2 p-2 rounded self-center">Ajouter au panier</button>
+          </ul>
+        ))}
+      </div>
+    </>
+  )
 }
