4f65f452c044ad1d67affd21099378b3f6d58366
Dykann Khaov <dykann.khaov@gmail.com>
2021-10-27 20:02:10 +0200

    finish integrating footer

---
+++ pages/store/Footer.tsx
@@ -0,0 +1,20 @@
+import { FaTwitter, FaFacebookF, FaInstagram } from 'react-icons/fa'
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center pt-2 bg-gray-800 text-white h-16">
+      Suivez-nous sur nos réseaux sociaux
+      <div className="flex text-2xl mt-1">
+        <a href="https://twitter.com/amazon" target="_blank" rel="noreferrer">
+          <FaTwitter className="cursor-pointer hover:text-gray-500" />
+        </a>
+        <a href="https://www.facebook.com/amazon" target="_blank" rel="noreferrer">
+          <FaFacebookF className="mx-6 cursor-pointer hover:text-gray-500" />
+        </a>
+        <a href="https://www.instagram.com/amazon" target="_blank" rel="noreferrer">
+          <FaInstagram className="cursor-pointer hover:text-gray-500" />
+        </a>
+      </div>
+    </footer>
+  )
+}
