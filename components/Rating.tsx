5345a5e8b219f038108a9debf6df0490a86b2b64
Dykann Khaov <dykann.khaov@gmail.com>
2021-11-10 00:42:39 +0100

    create a Rating component that will execute the convertTostars function by passing the item.rate props to Rating in the disPlayedProducts.map

---
+++ components/Rating.tsx
@@ -0,0 +1,16 @@
+import { convertToStars } from '../utils/helpers'
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+
+function Rating({ rate }) {
+  return (
+    <p className="flex">
+      {convertToStars(rate).map((star, index) => {
+        if (star === 'full') return <BsStarFill key={index} className="text-yellow-500" />
+        else if (star === 'half') return <BsStarHalf key={index} className="text-yellow-500" />
+        return <BsStar key={index} className="text-yellow-500" />
+      })}
+    </p>
+  )
+}
+
+export default Rating
