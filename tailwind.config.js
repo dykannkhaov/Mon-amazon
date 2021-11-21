c337a261c695b52e1433af83d080acab6ead116a
Dykann Khaov <dykann.khaov@gmail.com>
2021-11-21 22:02:01 +0100

    remove tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      spacing: {
-        '108': '27rem',
-        '120': '30rem',
-        '132': '33rem',
-        '144': '36rem',
-      },
     },
   },
   variants: {
