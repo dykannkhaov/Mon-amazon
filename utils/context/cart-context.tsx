1b71eb21a737210079a900ec9b86fef0939aa086
Dykann Khaov <dykann.khaov@gmail.com>
2021-11-29 17:30:09 +0100

    create a new item property, the quantity, when a same item is added to cart multiple times increase the quantity instead of creating a new line

--- utils/context/cart-context.tsx
+++ utils/context/cart-context.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react'
-import type { Product } from '../../types/product'
+import type { ProductCart } from '../../types/productCart'
 
 type CartProviderValue = {
-  cart: Array<Product>
-  addToCart: (item: Product) => void
-  deleteItemCart: (item: Product) => void
+  cart: Array<ProductCart>
+  cartLength: number
+  cartTotal: string
+  addToCart: (item: ProductCart) => void
+  deleteItemCart: (item: ProductCart) => void
   emptyCart: () => void
 }
 
 const context = React.createContext<CartProviderValue | undefined>(undefined)
 
 function CartProvider(props) {
-  const [cart, setCart] = React.useState<Array<Product>>(() => {
+  const [cart, setCart] = React.useState<Array<ProductCart>>(() => {
     if (typeof window === 'undefined') return []
     return JSON.parse(localStorage.getItem('cart')) ?? []
   })
@@ -20,15 +22,34 @@ function CartProvider(props) {
     localStorage.setItem('cart', JSON.stringify(cart))
   }, [cart])
 
-  const addToCart = (item: Product) => {
-    const newItem = {
-      ...item,
-      id: Date.now(),
-    }
-    setCart((oldCart) => [newItem, ...oldCart])
+  const cartLength = cart
+    .map((item) => item.quantity)
+    .reduce((previousQuantity, currentQuantity) => previousQuantity + currentQuantity, 0)
+
+  const cartTotal = cart
+    .map((item) => parseFloat(item.price) * item.quantity)
+    .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+    .toFixed(2)
+
+  const addToCart = (item: ProductCart) => {
+    setCart((oldCart) => {
+      const cartCopy = [...oldCart]
+      const matchedProductIndex = oldCart.findIndex((product) => product.name === item.name)
+      if (matchedProductIndex !== -1) {
+        cartCopy[matchedProductIndex].quantity++
+        return cartCopy
+      } else {
+        const newItem = {
+          ...item,
+          quantity: 1,
+          id: Date.now(),
+        }
+        return [...cartCopy, newItem].sort((oldItem, newItem) => (oldItem.id > newItem.id ? -1 : 1))
+      }
+    })
   }
 
-  const deleteItemCart = (item: Product) => {
+  const deleteItemCart = (item: ProductCart) => {
     const itemToDelete = item.id
     const newCart = cart.filter((item, index) => item.id !== itemToDelete)
     setCart(newCart)
@@ -38,7 +59,11 @@ function CartProvider(props) {
     setCart([])
   }
 
-  return <context.Provider value={{ cart, addToCart, deleteItemCart, emptyCart }}>{props.children}</context.Provider>
+  return (
+    <context.Provider value={{ cart, cartLength, cartTotal, addToCart, deleteItemCart, emptyCart }}>
+      {props.children}
+    </context.Provider>
+  )
 }
 
 function useCart() {
