8c303dd5effc8d031f10ba559b314f1323e8dd65
Dykann Khaov <dykann.khaov@gmail.com>
2022-03-07 18:54:24 +0100

    fix middleware conditions of redirections

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,27 +3,30 @@ import { products } from '../items/ProductsList'
 import { concatenateForUrl } from '../utils/concatenateForUrl'
 
 export function middleware(req: NextRequest) {
-  const isRoute = !req.url.includes('.')
+  const isRoute = !req.nextUrl.pathname.includes('.')
+  const url = req.nextUrl.clone()
+  url.pathname = '/'
 
   if (isRoute) {
     const categories = ['clothes', 'electronic', 'others']
     const productNames = products.map((product) => concatenateForUrl(product.name))
-    const isInCategory = categories.some((category) => req.url.endsWith(category))
-    const isAProduct = productNames.some((product) => req.url.endsWith(product))
+    const isInCategory = categories.some((category) => req.nextUrl.pathname.endsWith(category))
+    const isAProduct = productNames.some((product) => req.nextUrl.pathname.endsWith(product))
 
     // allow homepage redirection
-    if (req.url === '/') return NextResponse.next()
+    if (req.nextUrl.pathname === '/') return NextResponse.next()
 
     //  skip middleware if it requests an endpoint
-    if (req.url.startsWith('/api/')) return NextResponse.next()
+    if (req.nextUrl.pathname.startsWith('/api/')) return NextResponse.next()
 
     // redirect to cart if /cart
-    if (req.url === '/cart') return NextResponse.next()
+    if (req.nextUrl.pathname === '/cart') return NextResponse.next()
 
     // redirect to product page
-    if (req.url.startsWith('/description') && isAProduct) return NextResponse.next()
+    if (req.nextUrl.pathname.startsWith('/description') && isAProduct) return NextResponse.next()
 
-    // redirect to index when isInCategory === false and isAProduct === false
-    if (!isInCategory || (isInCategory && req.url.startsWith('/description'))) return NextResponse.redirect('/')
+    // redirect to index when !isInCategory and !isAProduct
+    if (!isInCategory || (isInCategory && req.nextUrl.pathname.startsWith('/description')))
+      return NextResponse.redirect(url)
   }
 }
